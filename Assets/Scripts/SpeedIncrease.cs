3ae80a266011a27e81511e343e718475a51225d8
Daniel Jaska <dtj190000@utdallas.edu>
2021-08-26 20:32:09 -0500

    V1

---
+++ Assets/Scripts/SpeedIncrease.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpeedIncrease : CollectibleBase
+{
+    [SerializeField] float _speedAmount = 5;
+
+    protected override void Collect(Player player)
+    {
+        //pull motor controller from player
+        TankController controller = player.GetComponent<TankController>();
+        if(controller != null)
+        {
+            controller.MoveSpeed += _speedAmount;
+        }
+    }
+
+    protected override void Movement(Rigidbody rb)
+    {
+        Quaternion turnOffset = Quaternion.Euler(MovementSpeed, MovementSpeed, MovementSpeed);
+    }
+}
